a9198c3bebd8fbd4e85eda23ba8c4a24dfe79704
Egor Ulianov <72970317+egor-ulianov@users.noreply.github.com>
2023-05-20 17:45:25 +0200

    FaceAPI integration

--- angular-snow/src/app/components/face-detector/face-detector.component.ts
+++ angular-snow/src/app/components/face-detector/face-detector.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FaceDetectorViewModel } from 'src/app/shared/view-models/face-detector.view-model';
+import * as faceapi from 'face-api.js';
 
 @Component({
   selector: 'app-face-detector',
@@ -11,18 +12,32 @@ export class FaceDetectorComponent implements OnInit
   //#region Properties
 
   public viewModel: FaceDetectorViewModel;
-  public video: HTMLVideoElement | null;
   public devicesList: MediaDeviceInfo[] | null;
+
+  @ViewChild('video',{ static: true })
+  public video: ElementRef | null;
+
+  @ViewChild('canvas',{ static: true })
+  public canvasRef: ElementRef | null;
+
+  public stream: any;
+  public detection: any;
+  public resizedDetections: any;
+  public canvas: any;
+  public canvasEl: any;
+  public displaySize: any;
+  public videoInput: any;
 
   //#endregion Properties
 
   //#region Constrcutors
 
-  constructor()
+  constructor(private elRef: ElementRef)
   {
     this.viewModel = new FaceDetectorViewModel();
-    this.video = null;
     this.devicesList = null;
+    this.canvasRef = null;
+    this.video = null;
   }
   
   //#endregion Constructors
@@ -31,8 +46,7 @@ export class FaceDetectorComponent implements OnInit
 
   public async ngOnInit(): Promise<void>
   {
-     this.video = document.getElementById('webcam') as HTMLVideoElement;
-     await this.startWebCam();
+     await this.loadModels();
   }
 
   //#endregion Angular lifecycle
@@ -41,12 +55,12 @@ export class FaceDetectorComponent implements OnInit
 
   public get cameraWidth(): number
   {
-    return window.innerWidth;
+    return this.viewModel.cameraWidth - 30;
   }
 
   public get cameraHeight(): number
   {
-    return window.innerHeight;
+    return this.viewModel.cameraHeight - 30;
   }
 
   //#endregion Getters and Setters
@@ -57,21 +71,75 @@ export class FaceDetectorComponent implements OnInit
   {
     this.devicesList = await navigator.mediaDevices.enumerateDevices();
 
-    navigator.mediaDevices.getUserMedia(
-      {
-        video : {facingMode: 'user'}, 
-        audio: false
+    if (this.video)
+      this.videoInput = this.video.nativeElement;
+
+    await navigator.mediaDevices.getUserMedia(
+      { 
+        video: {facingMode: 'user'}, 
+        audio: false 
       })
       .then((stream) => 
       {
-        if (this.video)
-          this.video.srcObject = stream;
+        this.videoInput.srcObject = stream;
+        let {width, height} = stream.getTracks()[0].getSettings();
+        this.viewModel.cameraWidth = width ?? 1;
+        this.viewModel.cameraHeight = height ?? 1;
       })
-      .catch(error =>
-        {
-          console.log('No camera');
-        })
+      .catch(
+        (err) => 
+        console.log(err)
+      );
+
+    this.viewModel.isCameraLoaded = true;
+    this.detectFaces();
   }
 
   //#endregion Public methods
+
+  //#region Private methods
+
+  private async loadModels(): Promise<void>
+  {
+    await Promise.all([faceapi.nets.tinyFaceDetector.loadFromUri('../../assets/models'),
+    await faceapi.nets.faceLandmark68Net.loadFromUri('../../assets/models'),
+    await faceapi.nets.faceRecognitionNet.loadFromUri('../../assets/models'),
+    await faceapi.nets.faceExpressionNet.loadFromUri('../../assets/models'),]).then(() => this.startWebCam());
+  }
+
+  private async detectFaces(): Promise<void> 
+  {
+    this.elRef.nativeElement.querySelector('video').addEventListener('play', async () => 
+      {
+        this.canvas = await faceapi.createCanvasFromMedia(this.videoInput);
+
+        if (this.canvasRef)
+          this.canvasEl = this.canvasRef.nativeElement;
+        this.canvasEl.appendChild(this.canvas);
+        this.canvas.setAttribute('id', 'canvass');
+        this.canvas.setAttribute(
+            'style',`position: relative;
+            top: 0;
+            left: 0;`
+        );
+        this.displaySize = {
+            width: this.videoInput.width,
+            height: this.videoInput.height,
+        };
+        faceapi.matchDimensions(this.canvas, this.displaySize);
+        setInterval(async () => {
+          this.detection = await faceapi.detectAllFaces(this.videoInput,  new  faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
+          this.resizedDetections = faceapi.resizeResults(
+              this.detection,
+              this.displaySize
+            );
+          this.canvas.getContext('2d').clearRect(0, 0,      this.canvas.width,this.canvas.height);
+          faceapi.draw.drawDetections(this.canvas, this.resizedDetections);
+          faceapi.draw.drawFaceLandmarks(this.canvas, this.resizedDetections);
+          faceapi.draw.drawFaceExpressions(this.canvas, this.resizedDetections);
+        }, 100);
+      });
+    }
+
+  //#endregion Private methods
 }
--- angular-snow/src/app/shared/view-models/face-detector.view-model.ts
+++ angular-snow/src/app/shared/view-models/face-detector.view-model.ts
@@ -1,4 +1,24 @@
 export class FaceDetectorViewModel
 {
-    
+    //#region Properties
+
+    public cameraWidth: number;
+
+    public cameraHeight: number;
+
+    public isCameraLoaded: boolean;
+
+    //#endregion Properties
+
+    //#region Constructors
+
+    constructor()
+    {
+        this.cameraHeight = 1;
+        this.cameraWidth = 1;
+        this.isCameraLoaded = false;
+    }
+
+    //#endregion Constructors
+
 }
